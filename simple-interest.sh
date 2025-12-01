7d3aacddc8dd5afd8d6b9e02ed433736d32bb9ac
geriko70 <134090275+geriko70@users.noreply.github.com>
2025-12-01 14:39:36 +0100

    Revise comments and author details in script
    Updated author information and added input/output comments.

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,4 +4,19 @@
    # Do not use this in production. Sample purpose only.
    # Author: Upkar Lidder (IBM)
    # Additional Authors:
-   # gerik70
+   # <your GitHub username>
+   # Input:
+   # p, principal amount
+   # t, time period in years
+   # r, annual rate of interest
+   # Output:
+   # simple interest = ptr
+   echo "Enter the principal:"
+   read p
+   echo "Enter rate of interest per year:"
+   read r
+   echo "Enter time period in years:"
+   read t
+   s=expr $p \* $t \* $r / 100
+   echo "The simple interest is: "
+   echo $s
